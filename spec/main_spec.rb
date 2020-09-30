ab84b2793a6578733ef32f65d7eff94b6436b144
joseabel <joseabel.ramirezfrontany@gmail.com>
2020-09-30 16:24:28 -0400

    Added Player class tests methods for sanitize_choice and refactor variables for other methods

---
+++ spec/main_spec.rb
@@ -0,0 +1,20 @@
+#spec/main_spec.rb
+require_relative '../bin/main.rb'
+
+describe "#game_confirmation" do
+
+  it "Should capture the input and downcase it" do
+    game_confirmation.to receive(:gets).and_return('foo')
+    name = game_confirmation.gets
+
+    expect(name).to eq('foo')
+  end
+end
+
+
+#   it "Should capture the input and downcase it" do
+#     expect do
+#       expect(game_confirmation('Yes')).to eq('Yes')
+#     end
+#   end
+# end
